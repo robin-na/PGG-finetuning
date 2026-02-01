2d14285913344983dc89c9bb4d001b59bc7ec64c
Robin Na <robin.w.na@gmail.com>
2026-01-31 20:30:04 -0500

    run simulation with persona

--- simulation.sh
+++ simulation.sh
@@ -30,6 +30,7 @@ echo "Running on $(hostname)"
 echo "Running on $(hostname)"
 nvidia-smi
 
-python Simulation_robin/run_simulation.py --provider local --base_model google/gemma-3-27b-it --use_peft False --include_reasoning True --env_csv Simulation_robin/df_analysis_val_19th.csv \
-    --persona random_full_transcript --output_root output_test
+python Simulation_robin/run_simulation.py --provider local --base_model google/gemma-3-27b-it --use_peft False --include_reasoning True \
+    --persona random_full_transcript --output_root output_persona
+
 
